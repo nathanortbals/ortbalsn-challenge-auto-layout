54a000aa0608e69190b82e7a5041effdcb43c6c1
Nathan Ortbals <nathan.ortbals@gmail.coma>
2019-03-10 12:02:39 -0500

    added files

--- ortbalsn-challenge-auto-layout/ViewController.swift
+++ ortbalsn-challenge-auto-layout/ViewController.swift
@@ -9,12 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var buttonOnTop: Bool = true
+    var topAnchor: NSLayoutConstraint? = nil
+    var bottomAnchor: NSLayoutConstraint? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Add button to view
+        let button  = UIButton()
+        button.backgroundColor = .green
+        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        self.view.addSubview(button)
+        
+        // Add left and right constraints
+        let margins = self.view.layoutMarginsGuide
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.leadingAnchor.constraint(equalTo: margins.leadingAnchor).isActive = true
+        button.trailingAnchor.constraint(equalTo: margins.trailingAnchor).isActive = true
+        
+        // Initialize and store constraints that will be added and deactivated
+        topAnchor = button.topAnchor.constraint(equalTo: margins.topAnchor)
+        bottomAnchor = button.topAnchor.constraint(equalTo: margins.bottomAnchor)
+        setButtonPosition(button: button)
     }
 
-
+    @objc func buttonAction(sender: UIButton!) {
+        buttonOnTop = !buttonOnTop
+        setButtonPosition(button: sender)
+    }
+    
+    func setButtonPosition(button: UIButton){
+        topAnchor?.isActive = self.buttonOnTop
+        bottomAnchor?.isActive = !self.buttonOnTop
+        
+        if(buttonOnTop) {
+            button.setTitle("Click me to move to the bottom", for: .normal)
+        }
+        else {
+            button.setTitle("Click me to move to the top", for: .normal)
+        }
+    }
 }
 
